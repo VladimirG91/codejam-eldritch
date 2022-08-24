4705f4ac9ee89726d66e1fb1ffdf8a57c7207d96
VladimirG91 <vladimir.glazkov91@gmail.com>
2022-08-24 11:23:19 +0600

    feat: add btn shuffle

--- src/index.js
+++ src/index.js
@@ -25,96 +25,108 @@ for (let i = 0; i < ancientsData.length; i++) {
   ancients.insertAdjacentHTML('beforeend', divTag)
 }
 const ancientImgList = document.querySelectorAll('.ancient-img')
-ancientImgList.forEach((el) =>
-  el.addEventListener('click', () => {
-    el.classList.toggle('active')
-  })
-)
+const difficultyContainer = document.querySelector('.difficulty-container')
+const shuffleBtn = document.querySelector('.shuffle-btn')
+
+for (var i = 0; i < ancientImgList.length; i++) {
+  ancientImgList[i].onclick = function () {
+    if (this.classList) {
+      for (var j = 0; j < ancientImgList.length; j++) {
+        ancientImgList[j].classList.remove('active')
+      }
+      this.classList.add('active')
+      difficultyContainer.classList.remove('hide')
+      shuffleBtn.classList.remove('hide')
+    } else {
+      this.active += ' ' + active
+    }
+  }
+}
 
 // let blue1 = `<img class="blue1" src="${greenCardsData[0].cardFace}" alt="${greenCardsData[0].id}"/>`
 // cards.insertAdjacentHTML('beforeend', blue1)
 // получаем все карты
-const allCards = [...blueCardsData, ...brownCardsData, ...greenCardsData]
-console.log('allCards >', allCards)
-// получаем массивы из легких, обычных, сложных карт
-let allEasyCards = []
-let allNormalCards = []
-let allHardCards = []
-allCards.forEach((el) => {
-  if (el.difficulty == 'easy') {
-    allEasyCards.push(el)
-  }
-  if (el.difficulty == 'normal') {
-    allNormalCards.push(el)
-  }
-  if (el.difficulty == 'hard') {
-    allHardCards.push(el)
-  }
-})
-// получаем массивы EASE карт по цветам
-let allEasyGreenCards = []
-let allEasyBlueCards = []
-let allEasyBrownCards = []
+// const allCards = [...blueCardsData, ...brownCardsData, ...greenCardsData]
+// console.log('allCards >', allCards)
+// // получаем массивы из легких, обычных, сложных карт
+// let allEasyCards = []
+// let allNormalCards = []
+// let allHardCards = []
+// allCards.forEach((el) => {
+//   if (el.difficulty == 'easy') {
+//     allEasyCards.push(el)
+//   }
+//   if (el.difficulty == 'normal') {
+//     allNormalCards.push(el)
+//   }
+//   if (el.difficulty == 'hard') {
+//     allHardCards.push(el)
+//   }
+// })
+// // получаем массивы EASE карт по цветам
+// let allEasyGreenCards = []
+// let allEasyBlueCards = []
+// let allEasyBrownCards = []
 
-allEasyCards.forEach((el) => {
-  if (el.color == 'green') {
-    allEasyGreenCards.push(el)
-  }
-  if (el.color == 'blue') {
-    allEasyBlueCards.push(el)
-  }
-  if (el.color == 'brown') {
-    allEasyBrownCards.push(el)
-  }
-})
+// allEasyCards.forEach((el) => {
+//   if (el.color == 'green') {
+//     allEasyGreenCards.push(el)
+//   }
+//   if (el.color == 'blue') {
+//     allEasyBlueCards.push(el)
+//   }
+//   if (el.color == 'brown') {
+//     allEasyBrownCards.push(el)
+//   }
+// })
 
-// получаем массивы NORMAL карт по цветам
-let allNormalGreenCards = []
-let allNormalBlueCards = []
-let allNormalBrownCards = []
+// // получаем массивы NORMAL карт по цветам
+// let allNormalGreenCards = []
+// let allNormalBlueCards = []
+// let allNormalBrownCards = []
 
-allNormalCards.forEach((el) => {
-  if (el.color == 'green') {
-    allNormalGreenCards.push(el)
-  }
-  if (el.color == 'blue') {
-    allNormalBlueCards.push(el)
-  }
-  if (el.color == 'brown') {
-    allNormalBrownCards.push(el)
-  }
-})
+// allNormalCards.forEach((el) => {
+//   if (el.color == 'green') {
+//     allNormalGreenCards.push(el)
+//   }
+//   if (el.color == 'blue') {
+//     allNormalBlueCards.push(el)
+//   }
+//   if (el.color == 'brown') {
+//     allNormalBrownCards.push(el)
+//   }
+// })
 
-// получаем массивы HARD карт по цветам
-let allHardGreenCards = []
-let allHardBlueCards = []
-let allHardBrownCards = []
+// // получаем массивы HARD карт по цветам
+// let allHardGreenCards = []
+// let allHardBlueCards = []
+// let allHardBrownCards = []
 
-allHardCards.forEach((el) => {
-  if (el.color == 'green') {
-    allHardGreenCards.push(el)
-  }
-  if (el.color == 'blue') {
-    allHardBlueCards.push(el)
-  }
-  if (el.color == 'brown') {
-    allHardBrownCards.push(el)
-  }
-})
+// allHardCards.forEach((el) => {
+//   if (el.color == 'green') {
+//     allHardGreenCards.push(el)
+//   }
+//   if (el.color == 'blue') {
+//     allHardBlueCards.push(el)
+//   }
+//   if (el.color == 'brown') {
+//     allHardBrownCards.push(el)
+//   }
+// })
 
-console.log('allEasyCards >', allEasyCards)
-console.log('allNormalCards >', allNormalCards)
-console.log('allHardCards >', allHardCards)
-//
-console.log('allEasyGreenCards >', allEasyGreenCards)
-console.log('allEasyBlueCards >', allEasyBlueCards)
-console.log('allEasyBrownCards >', allEasyBrownCards)
-//
-console.log('allNormalGreenCards >', allNormalGreenCards)
-console.log('allNormalBlueCards >', allNormalBlueCards)
-console.log('allNormalBrownCards >', allNormalBrownCards)
-//
-console.log('allHardGreenCards >', allHardGreenCards)
-console.log('allHardBlueCards >', allHardBlueCards)
-console.log('allHardBrownCards >', allHardBrownCards)
-//
+// console.log('allEasyCards >', allEasyCards)
+// console.log('allNormalCards >', allNormalCards)
+// console.log('allHardCards >', allHardCards)
+// //
+// console.log('allEasyGreenCards >', allEasyGreenCards)
+// console.log('allEasyBlueCards >', allEasyBlueCards)
+// console.log('allEasyBrownCards >', allEasyBrownCards)
+// //
+// console.log('allNormalGreenCards >', allNormalGreenCards)
+// console.log('allNormalBlueCards >', allNormalBlueCards)
+// console.log('allNormalBrownCards >', allNormalBrownCards)
+// //
+// console.log('allHardGreenCards >', allHardGreenCards)
+// console.log('allHardBlueCards >', allHardBlueCards)
+// console.log('allHardBrownCards >', allHardBrownCards)
+// //
