d43831cf20720cbc8bf8fa382d433969b17a7858
VladimirG91 <vladimir.glazkov91@gmail.com>
2022-08-25 00:24:56 +0600

    feat: add func shuffle, add, deckbtn, count func

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import allBlueCards from './data/mythicCards/blue/index'
 import allBrownCards from './data/mythicCards/brown/index'
 import allGreenCards from './data/mythicCards/green/index'
 
-console.log('dificulties >', difficulties)
-console.log('ancientsData >', ancientsData)
+// console.log('dificulties >', difficulties)
+// console.log('ancientsData >', ancientsData)
 
 const ancients = document.querySelector('.ancients')
 const cards = document.querySelector('.cards')
+
+// функция рандомайзер
+function numRandomized() {
+  return 0.5 - Math.random()
+}
+function shuffleCards(arr) {
+  return [...arr].sort(numRandomized)
+}
 
 for (let i = 0; i < ancientsData.length; i++) {
   let divTag = `<div class="ancient_div">
@@ -26,12 +34,96 @@ const difficultyContainer = document.querySelector('.difficulty-container')
 const shuffleBtn = document.querySelector('.shuffle-btn')
 const counter = document.querySelector('.counter-container ')
 let allDots = document.querySelectorAll('.dot')
+const deckBtn = document.querySelector('.deck')
 
 let azathothCount = [1, 2, 1, 3, 2, 1, 2, 4, 0]
 let cthulhuCount = [0, 2, 2, 1, 3, 0, 3, 4, 0]
 let iogSothothCount = [0, 2, 1, 2, 3, 1, 3, 4, 0]
 let shubNiggurathCount = [1, 2, 1, 3, 2, 1, 2, 4, 0]
 
+let azathothFirstStage = []
+let azathothSecondStage = []
+let azathothThirdStage = []
+// ==========функция шафл ======================
+shuffleBtn.addEventListener('click', () => {
+  // шафл грин
+  let shuffledAllGreenArr = shuffleCards(allGreenCards),
+    shuffledEasyGreenArr = shuffleCards(allEasyGreenCards),
+    shuffledNormalGreenArr = shuffleCards(allNormalGreenCards),
+    shuffledHardGreenArr = shuffleCards(allHardGreenCards)
+  // шафл браун
+  let shuffledAllBrownArr = shuffleCards(allBrownCards),
+    shuffledEasyBrownArr = shuffleCards(allEasyBrownCards),
+    shuffledNormalBrownArr = shuffleCards(allNormalBrownCards),
+    shuffledHardBrownArr = shuffleCards(allHardBrownCards)
+  // шафл блю
+  let shuffledAllBlueArr = shuffleCards(allBlueCards),
+    shuffledEasyBlueArr = shuffleCards(allEasyBlueCards),
+    shuffledNormalBlueArr = shuffleCards(allNormalBlueCards),
+    shuffledHardBlueArr = shuffleCards(allHardBlueCards)
+
+  deckBtn.classList.remove('hide')
+  if (
+    ancientImgList[0].classList.contains('active') &&
+    difficulty[2].classList.contains('act')
+  ) {
+    for (let i = 0; i < 1; i++) {
+      azathothFirstStage.push(shuffledAllGreenArr.pop())
+      azathothFirstStage.push(shuffledAllBlueArr.pop())
+      azathothSecondStage.push(shuffledAllBlueArr.pop())
+    }
+    for (let i = 0; i < 2; i++) {
+      azathothFirstStage.push(shuffledAllBrownArr.pop())
+      azathothSecondStage.push(shuffledAllBrownArr.pop())
+      azathothThirdStage.push(shuffledAllGreenArr.pop())
+    }
+    for (let i = 0; i < 3; i++) {
+      azathothSecondStage.push(shuffledAllGreenArr.pop())
+    }
+    for (let i = 0; i < 4; i++) {
+      azathothThirdStage.push(shuffledAllBrownArr.pop())
+    }
+    console.log('azathothFirstStage', azathothFirstStage)
+    console.log('azathothSecondStage', azathothSecondStage)
+    console.log('azathothThirdStage', azathothThirdStage)
+  }
+})
+
+// функция подсчета карт в стэйжах
+function countFunc(arr) {
+  let countGreen = 0
+  let countBrown = 0
+  let countBlue = 0
+
+  arr.forEach((el) => {
+    if (el['color'] == 'green') {
+      countGreen++
+    }
+    if (el['color'] == 'brown') {
+      countBrown++
+    }
+    if (el['color'] == 'blue') {
+      countBlue++
+    }
+  })
+
+  console.log(countGreen)
+  console.log(countBrown)
+  console.log(countBlue)
+}
+
+deckBtn.addEventListener('click', () => {
+  if (ancientImgList[0].classList.contains('active')) {
+    countFunc(azathothFirstStage)
+    // countFunc(azathothSecondStage),
+    // countFunc(azathothThirdStage)
+  }
+  if (azathothFirstStage.length > 0) {
+    azathothFirstStage.pop()
+    console.log('length', azathothFirstStage.length)
+  }
+})
+// слудующий шаг запушить все в 1 массив и методом поп удалять при нажатии на дукбтн
 // добавляем класс active древнему по клику
 for (var i = 0; i < ancientImgList.length; i++) {
   ancientImgList[i].onclick = function () {
@@ -170,27 +262,6 @@ console.log('allHardBlueCards >', allHardBlueCards)
 console.log('allHardBrownCards >', allHardBrownCards)
 //
 
-// let allGreenCards = []
-// let allBlueCards = []
-// let allBrownCards = []
-
-// allCards.forEach((el) => {
-//   if (el.color == 'green') {
-//     allGreenCards.push(el)
-//   }
-//   if (el.color == 'blue') {
-//     allBlueCards.push(el)
-//   }
-//   if (el.color == 'brown') {
-//     allBrownCards.push(el)
-//   }
-// })
 console.log('allGreenCards >', allGreenCards)
 console.log('allBlueCards >', allBlueCards)
 console.log('allBrownCards >', allBrownCards)
-function numRandomized() {
-  return 0.5 - Math.random()
-}
-function shuffleCards(arr) {
-  return [...arr].sort(numRandomized)
-}
